b08624908b294c2072f432a6af1a95a32014ae30
Sehyun Kim <sehyun.seankim@gmail.com>
2023-10-25 11:54:43 +0900

    input data dimension issues handled in align~.py

--- alignment_search_eval_fid300.py
+++ alignment_search_eval_fid300.py
@@ -1,7 +1,6 @@
 import torch
 import cv2
 import numpy as np
-from scipy.ndimage import binary_erosion, rotate
 # from scipy.io import loadmat, savemat
 import scipy.io as sio
 import os
@@ -30,21 +29,27 @@ def alignment_search_eval_fid300(p_inds, db_ind=2):
     db_save_dir = os.path.join('feats', dbname)
     first_feat_path = os.path.join(db_save_dir, 'fid300_001.pkl')
     
+    # pickle.load(f) returns a dictionary which has
+    # db_feats, db_labels, feat_dims, rfsIm, trace_H, trace_W as keys
     with open(first_feat_path, 'rb') as f:
-        db_feats_init, feat_dims, rfsIm, trace_H, trace_W = pickle.load(f)
+        fid300_001 = pickle.load(f)
+        db_feats_init = fid300_001['db_feats']
+        feat_dims = fid300_001['feat_dims']
+        rfsIm = fid300_001['rfsIm']
+        trace_H = fid300_001['trace_H']
+        trace_W = fid300_001['trace_W']
     
+    # len(db_chunks) is 1175
     db_chunk_inds = db_chunks[0]
+    # db_feats.shape = (1175, 256, 2, 1175)
     db_feats = np.zeros((db_feats_init.shape[0], db_feats_init.shape[1], 
                      db_feats_init.shape[2], len(db_chunk_inds)), dtype=db_feats_init.dtype)
 
     # Loading specified chunks of the database and filling the db_feats array
-    # for i, ind in enumerate(db_chunk_inds):
-    #     filename = os.path.join('feats', dbname, f'fid300_{ind:03d}.mat')
-    #     dat = sio.loadmat(filename)
-    #     db_feats[:, :, :, i] = dat['db_feats']
     for i, ind in enumerate(db_chunk_inds):
         filename = os.path.join('feats', dbname, f'fid300_{ind:03d}.pkl')
-        dat = pickle.load(filename)
+        with open(filename, 'rb') as filename:
+            dat = pickle.load(filename)
         db_feats[:, :, :, i] = dat['db_feats']
 
     im_f2i = feat_2_image(rfsIm)
@@ -53,8 +58,10 @@ def alignment_search_eval_fid300(p_inds, db_ind=2):
     se = np.ones((radius, radius))
 
     ones_w = torch.ones((1, 1, feat_dims[3]), dtype=torch.float32).cuda()
-
-    # db_feats to gpu, etc. 
+    
+    # First, 'results/<dbnmae>' path needs to be created
+    if not os.path.exists(os.path.join('results', dbname)):
+        os.makedirs(os.path.join('results', dbname), exist_ok=True)
     
     # p_inds = [start, end]
     for p in range(p_inds[0], p_inds[1]+1):
@@ -65,13 +72,15 @@ def alignment_search_eval_fid300(p_inds, db_ind=2):
         if os.path.exists(lock_fname):
             continue
         
-        fid = open(lock_fname, 'a')
+        # if the file does not exist, a+ option creates it ('a' option assumes the file exists)
+        fid = open(lock_fname, 'a+')
         fid.write(f'p={time.time()}')
         
         # Read and resize the image
-        p_im = cv2.imread(os.path.join('datasets', 'FID-300', 'tracks_cropped', f'{p:05d}.jpg'))
+        # We need 2D dimension numpy array for p_im (in MATLAB code)
+        p_im = cv2.imread(os.path.join('datasets', 'FID-300', 'tracks_cropped', f'{p:05d}.jpg'), cv2.IMREAD_GRAYSCALE)
         p_im = cv2.resize(p_im, (0, 0), fx=imscale, fy=imscale)
-        p_H, p_W, p_C = p_im.shape
+        p_H, p_W = p_im.shape
 
         # Fix latent prints are bigger than the test impressions
         if p_H > p_W and p_H > trace_H:
@@ -81,15 +90,14 @@ def alignment_search_eval_fid300(p_inds, db_ind=2):
         
         # Subtract mean_im_pix from p_im
         # p_im = p_im.astype(np.float32) - mean_im_pix
-        
-        
-        p_H, p_W, p_C = p_im.shape
+        p_H, p_W = p_im.shape
         
         # Pad the latent print
         pad_H = trace_H - p_H
         pad_W = trace_W - p_W
-
+        
         # Padding p_im and a logical ones matrix
+        # p_im.shape = (H, W, 3) -> 3D. In MATLAB code, it is 2D.
         p_im_padded = np.pad(p_im, ((pad_H, pad_H), (pad_W, pad_W)), mode='constant', constant_values=255)
         p_mask_padded = np.pad(np.ones((p_H, p_W), dtype=bool), ((pad_H, pad_H), (pad_W, pad_W)), mode='constant', constant_values=0)
 
@@ -102,13 +110,6 @@ def alignment_search_eval_fid300(p_inds, db_ind=2):
 
         # Initializing scores_ones with zeros
         scores_ones = np.zeros((len(transy), len(transx), len(angles), db_feats.shape[3]), dtype=np.float32)
-        # In this code:
-
-        # np.pad() function is used to pad the array, which corresponds to the MATLAB padarray() function.
-        # np.arange() is used for creating number sequences, similar to MATLAB’s colon operator :.
-        # np.zeros() is used to initialize an array of zeros, with the shape determined by the length of transy, transx, angles, and the fourth dimension size of db_feats.
-        # The datatype np.float32 is used for creating the zero array, which corresponds to MATLAB’s 'single'.
-        # Boolean dtype (dtype=bool) is used for creating a logical ones matrix.
 
         cnt = 0
         eraseStr = ''
@@ -129,8 +130,6 @@ def alignment_search_eval_fid300(p_inds, db_ind=2):
             
             for offsetx in offsets_x:
                 for offsety in offsets_y:
-                    
-                    # This part needs the actual implementation of the function generate_db_CNNfeats_gpu
                     p_r_feat = generate_db_CNNfeats_gpu(net, p_im_padded_r[offsety:, offsetx:, :])
                     
                     for j in range(p_r_feat.shape[1] - feat_dims[2] + 1):
@@ -152,9 +151,7 @@ def alignment_search_eval_fid300(p_inds, db_ind=2):
                             p_ijr_feat = p_r_feat[i:i+feat_dims[1], j:j+feat_dims[2], :]
                             p_mask_ijr = p_mask_padded_r[pix_i:pix_i+trace_H, pix_j:pix_j+trace_W]
                             
-                            p_ijr_feat_mask = warp_masks(p_mask_ijr, im_f2i, feat_dims, db_ind)  # Placeholder
-                            
-                            # Assuming radius and se are predefined
+                            p_ijr_feat_mask = warp_masks(p_mask_ijr, im_f2i, feat_dims, db_ind) # Placeholder
                             p_ijr_feat_mask = cv2.copyMakeBorder(p_ijr_feat_mask, radius, radius, radius, radius, cv2.BORDER_CONSTANT, value=0)
                             p_ijr_feat_mask = cv2.erode(p_ijr_feat_mask, se)
                             p_ijr_feat_mask = p_ijr_feat_mask[radius:-radius, radius:-radius, :]
@@ -164,22 +161,12 @@ def alignment_search_eval_fid300(p_inds, db_ind=2):
                             scores_ones[int(pix_i/2+0.5), int(pix_j/2+0.5), r, :] = scores_cell[0]
                             cnt += 1
             
-        # minsONES = max(max(max(scores_ones, [], 1), [], 2), [], 3);
-        # locaONES = bsxfun(@eq, scores_ones, minsONES);
-
-        # % save results
-        # save_results(fname, struct('scores_ones', scores_ones, ...
-        #                             'minsONES', minsONES, ...
-        #                             'locaONES', locaONES));
         minsONES = np.max(np.max(np.max(scores_ones, axis=0), axis=0), axis=0)
         locaONES = scores_ones == minsONES
         np.savez(fname, scores_ones, minsONES, locaONES)
-        
-        # remove lockfile
-        # close fid file
+
         fid.close()
         os.remove(lock_fname)
-
 
 # Some additional functions might need to be translated or imported, such as:
 # - get_db_attrs
--- utils_custom/feat_2_image.py
+++ utils_custom/feat_2_image.py
@@ -1,14 +1,9 @@
 import numpy as np
 from skimage.transform import estimate_transform
-
-class RFStruct:
-    def __init__(self, size, stride, offset):
-        self.size = size
-        self.stride = stride
-        self.offset = offset
+from getVarReceptiveFields_custom import RecetiveField
 
 def feat_2_image(rfs):
-    if isinstance(rfs, RFStruct):
+    if isinstance(rfs, RecetiveField):
         rfs = [rfs]
 
     feat_coords_x1 = [-100, -100, +100, +100]
@@ -16,12 +11,16 @@ def feat_2_image(rfs):
     feat_coords_x2 = [-100, -100, +100, +100]
     feat_coords_y2 = [-100, +100, -100, +100]
     
+    # In MATLAB code, rfs was 1*n struct array but here it is just a single instance
     for rf in rfs:
         if isinstance(rf, list) and len(rf) > 1:
             rf = rf[-1]
 
         feat_coords_x2 = rf.stride[1] * (np.array(feat_coords_x2) - 1) + rf.offset[1]
         feat_coords_y2 = rf.stride[0] * (np.array(feat_coords_y2) - 1) + rf.offset[0]
+    
+    # feat_coords_x2 = rfs.stride[1] * (np.array(feat_coords_x2) - 1) + rfs.offset[1]
+    # feat_coords_y2 = rfs.stride[0] * (np.array(feat_coords_y2) - 1) + rfs.offset[0]
 
     src = np.column_stack((feat_coords_x1, feat_coords_y1))
     dst = np.column_stack((feat_coords_x2, feat_coords_y2))
@@ -29,8 +28,3 @@ def feat_2_image(rfs):
     tform = estimate_transform('affine', src, dst)
     
     return tform.params
-
-# Example usage:
-rfs_example = [RFStruct(size=[11, 11], stride=[4, 4], offset=[3, 3])] * 27  # 1x27 struct array
-transformation_matrix = feat_2_image(rfs_example)
-print(transformation_matrix)
